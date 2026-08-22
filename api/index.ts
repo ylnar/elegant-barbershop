d41be43aa3d626707866a56607196a7eed485872
Jimmy <piubitt@gmail.com>
2026-08-22 19:43:51 +0700

    cc

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,6 @@
 import 'dotenv/config';
-import serverless from 'serverless-http';
 import { createApp } from '../server/app.ts';
 
 const app = createApp();
-const wrappedHandler = serverless(app);
 
-export default function handler(req: unknown, res: unknown) {
-  return wrappedHandler(req, res);
-}
+export default app;
